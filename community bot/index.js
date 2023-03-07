171e0317f730d59cdac27b0542ec3039430eed92
thomasemming <t.emming@hotmail.com>
2023-03-07 14:56:55 +0100

    part-2

--- community bot/index.js
+++ community bot/index.js
@@ -1,6 +1,24 @@
 require('dotenv').config();
+
+const fs = require('node:fs');
+const path = require('node:path');
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
