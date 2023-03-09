7304a1b9f0bbb719a1781120eb13f04499df1b32
thomasemming <t.emming@hotmail.com>
2023-03-09 10:46:38 +0100

    part-2.3

--- community bot/index.js
+++ community bot/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
@@ -20,8 +20,31 @@ for (const file of commandFiles) {
 	}
 }
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.login(process.env.CLIENT_TOKEN);
